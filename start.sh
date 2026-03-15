c9fdc3d7bf8c5ee05c8261258affef794828ce6c
Felix <52862870+devilAPI@users.noreply.github.com>
2026-03-15 18:29:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Create required s6 runtime directories
 mkdir -p /var/run/s6/services/s6-fdholderd/supervise
+chown -R container:container /mnt/server
 # You may add more directories if other services fail, but start with this.
 exec /init
